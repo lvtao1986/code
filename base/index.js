428dfefc623aa75437fd02c125f6e291a3d6ee9e
lvtao1986 <lvtao_java@163.com>
2016-12-20 13:28:59 +0800

    Update index.js

--- base/index.js
+++ base/index.js
@@ -1,4 +1,4 @@
 var n = Math.random();
 if(true){
-    _RESULT = jade.render("h Hello \nscript !{script}", { script: 'alert(' + n + ')'}));
+    _RESULT = jade.render("h Hello \nscript !{script}", { script: 'alert(' + n + ')'});
 }
