a93788324a7643caf73b2bdcfe9957175affc46f
lvtao1986 <lvtao_java@163.com>
2016-12-20 14:49:10 +0800

    Update index.js

--- base/index.js
+++ base/index.js
@@ -1,3 +1,6 @@
 sendHttp('index.jade', function(str, res){
     sendstr(jade.render(str),res);
 }, res);
+io.sockets.on('connection', function (socket) {    
+      socket.broadcast.emit('display',  "alert(hello)");  
+});
